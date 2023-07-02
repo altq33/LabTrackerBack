723732a9ac41013dc39c30d118e3ced7e35025e3
Дмитрий Чанков <chankovbelka123@yandex.ru>
2023-07-02 12:48:33 +0400

    change another one getting .env vars part on pydantic model

--- migrations/env.py
+++ migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from config import DB_HOST, DB_USER, DB_NAME, DB_PASS, DB_PORT
+from config import settings
 from models.models import metadata
 
 # this is the Alembic Config object, which provides
@@ -13,11 +13,11 @@ from models.models import metadata
 config = context.config
 
 section = config.config_ini_section
-config.set_section_option(section, "DB_HOST", DB_HOST)
-config.set_section_option(section, "DB_USER", DB_USER)
-config.set_section_option(section, "DB_NAME", DB_NAME)
-config.set_section_option(section, "DB_PASS", DB_PASS)
-config.set_section_option(section, "DB_PORT", DB_PORT)
+config.set_section_option(section, "DB_HOST", settings.db_host)
+config.set_section_option(section, "DB_USER", settings.db_user)
+config.set_section_option(section, "DB_NAME", settings.db_name)
+config.set_section_option(section, "DB_PASS", settings.db_pass)
+config.set_section_option(section, "DB_PORT", settings.db_port)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
