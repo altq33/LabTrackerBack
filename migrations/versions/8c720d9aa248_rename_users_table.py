6afd958b73714caaa1ad427936153049135b977b
Dmitry Chankov <chankovbelka123@yandex.ru>
2023-07-03 16:26:12 +0400

    add migrations

--- migrations/versions/8c720d9aa248_rename_users_table.py
+++ migrations/versions/8c720d9aa248_rename_users_table.py
@@ -1,8 +1,8 @@
-"""rename and refactor user model
+"""rename users table
 
-Revision ID: 9649489b7c4a
+Revision ID: 8c720d9aa248
 Revises: 415c57e4d149
-Create Date: 2023-07-02 20:43:32.481437
+Create Date: 2023-07-02 15:04:35.188554
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9649489b7c4a'
+revision = '8c720d9aa248'
 down_revision = '415c57e4d149'
 branch_labels = None
 depends_on = None
