76850032c7bce29f2586c1fa98e446faebac55bc
Дмитрий Чанков <chankovbelka123@yandex.ru>
2023-07-12 16:46:35 +0400

    fix db tracker models and make migrations for them

--- src/tracker/models.py
+++ src/tracker/models.py
@@ -1,7 +1,9 @@
 import uuid
 
 from sqlalchemy import String, TIMESTAMP, Column, UUID, MetaData, Text, ForeignKey, SmallInteger
+from sqlalchemy.orm import relationship
 
+from src.auth.models import User
 from src.database import Base
 
 """DB MODELS"""
@@ -18,7 +20,8 @@ class Task(Base):
     deadline = Column(TIMESTAMP, default=None)
     description = Column(Text, default=None)
     type = Column(String, default=None)
-    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    priority = Column(String, default="standart")
+    user_id = Column(UUID(as_uuid=True), ForeignKey(User.id, ondelete='CASCADE', ), nullable=False)
     subject_id = Column(UUID(as_uuid=True), ForeignKey('subjects.id', ondelete='CASCADE'), nullable=False)
 
 
@@ -31,14 +34,15 @@ class Teacher(Base):
     surname = Column(String(length=100), nullable=False)
     father_name = Column(String(length=100), nullable=True, default=None)
     phone_number = Column(Text, default=None)
-    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey(User.id, ondelete='CASCADE'), nullable=False)
 
 
 class Subject(Base):
     __tablename__ = 'subjects'
     metadata = metadata
+
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     name = Column(String(length=100), nullable=False)
     course = Column(SmallInteger, default=None)
     teacher_id = Column(UUID(as_uuid=True), ForeignKey('teachers.id', ondelete='CASCADE'), nullable=False)
-    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey(User.id, ondelete='CASCADE'), nullable=False)
