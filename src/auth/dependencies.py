6fb7d17eb0f444af287626623079e951b1963d40
Дмитрий Чанков <chankovbelka123@yandex.ru>
2023-07-17 16:46:04 +0400

    add DI logic for auth module validation

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -1,16 +1,18 @@
 from typing import Annotated
 from uuid import UUID
 
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, Path
 from jose import jwt, JWTError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.auth.exceptions import credentials_exception
-from src.auth.schemas import Roles, TokenData
+from src.auth.schemas import Roles, TokenData, UserInDB
 from src.auth.service import get_user_by_username, oauth2_scheme, get_user_by_id
+from src.auth.utils import check_access_permissions
 from src.config import settings
 from src.database import get_session
+from src.exceptions import not_found_exception
+from src.tracker.exceptions import not_enough_permissions_exception as tracker_not_enough_permissions_exception
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)],
@@ -29,3 +31,15 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)],
         raise credentials_exception
     return user
 
+
+async def check_user_id(user_id: Annotated[UUID, Path()], db_session: Annotated[AsyncSession, Depends(get_session)]):
+    user = await get_user_by_id(user_id, db_session)
+    if not user:
+        raise not_found_exception
+    return user
+
+
+async def check_access(target_user: Annotated[UserInDB, Depends(check_user_id)],
+                       current_user: Annotated[UserInDB, Depends(get_current_user)]):
+    if not check_access_permissions(current_user, target_user):
+        raise tracker_not_enough_permissions_exception
--- src/auth/router.py
+++ src/auth/router.py
@@ -3,18 +3,17 @@ from typing import Annotated
 from uuid import UUID
 
 from fastapi import APIRouter, Depends, Body
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.auth.dependencies import get_current_user
+from src.auth.dependencies import get_current_user, check_user_id, check_access
 from src.auth.exceptions import already_busy_exception, incorrect_auth_data_exception
 from src.auth.schemas import CreateUser, ShowUser, ShowDeletedUser, ShowUpdatedUser, UpdateUserRequest, Token, UserInDB
 from src.auth.service import create_new_user, delete_user_by_id, get_user_by_id, update_user, authenticate_user
-from src.auth.utils import create_access_token, check_access_permissions
+from src.auth.utils import create_access_token
 from src.config import settings
 from src.database import get_session
-from src.exceptions import not_found_exception, empty_body_exception
-from src.tracker.exceptions import not_enough_permissions_exception as tracker_not_enough_permissions_exception
+from src.exceptions import empty_body_exception
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -28,16 +27,9 @@ async def registration(user: Annotated[CreateUser, Body(title='Registration body
     return new_user
 
 
-@router.delete("/{user_id}", response_model=ShowDeletedUser)
-async def delete_user(user_id: UUID, session: Annotated[AsyncSession, Depends(get_session)],
-                      current_user: Annotated[UserInDB, Depends(get_current_user)]) -> ShowDeletedUser:
-    target_user = await get_user_by_id(user_id, session)
-    if not target_user:
-        raise not_found_exception
-    if check_access_permissions(current_user, target_user):
-        deleted_user = await delete_user_by_id(user_id, session)
-    else:
-        raise tracker_not_enough_permissions_exception
+@router.delete("/{user_id}", response_model=ShowDeletedUser, dependencies=[Depends(check_user_id), Depends(check_access)])
+async def delete_user(user_id: UUID, session: Annotated[AsyncSession, Depends(get_session)]) -> ShowDeletedUser:
+    deleted_user = await delete_user_by_id(user_id, session)
     return deleted_user
 
 
@@ -48,30 +40,21 @@ async def read_users_me(
     return current_user
 
 
-@router.get("/{user_id}", response_model=ShowUser)
+@router.get("/{user_id}", response_model=ShowUser, dependencies=[Depends(check_user_id)])
 async def get_user(user_id: UUID, session: AsyncSession = Depends(get_session)) -> ShowUser:
     user = await get_user_by_id(user_id, session)
-    if not user:
-        raise not_found_exception
     return user
 
 
-@router.patch("/{user_id}", response_model=ShowUpdatedUser)
+@router.patch("/{user_id}", response_model=ShowUpdatedUser, dependencies=[Depends(check_user_id), Depends(check_access)])
 async def update_user_by_id(user_id: UUID, body: UpdateUserRequest,
                             session: Annotated[AsyncSession, Depends(get_session)],
-                            current_user: Annotated[UserInDB, Depends(get_current_user)]
                             ) -> ShowUpdatedUser:
-    target_user = await get_user_by_id(user_id, session)
-    if not target_user:
-        raise not_found_exception
-    if check_access_permissions(current_user, target_user):
-        if body.dict(exclude_none=True) == {}:
-            raise empty_body_exception
-        updated_user = await update_user(user_id, body, session)
-        if not updated_user:
-            raise already_busy_exception
-    else:
-        raise tracker_not_enough_permissions_exception
+    if body.dict(exclude_none=True) == {}:
+        raise empty_body_exception
+    updated_user = await update_user(user_id, body, session)
+    if not updated_user:
+        raise already_busy_exception
     return updated_user
 
 
